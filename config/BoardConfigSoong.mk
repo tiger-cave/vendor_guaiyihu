08cfa15f696ccde52887eb35ab79ee68e5ccf294
Bruno Martins <bgcngm@gmail.com>
2018-12-28 21:34:03 +0000

    qcom: Mark some gralloc bits as valid
     * Add GRALLOC_USAGE_PRIVATE_WFD as valid gralloc bits for all
       the platforms and GRALLOC_USAGE_PRIVATE_10BIT_TP only for the
       UM 4.9-based platforms. Both are defined in the respective
       display HALs (in gralloc/gralloc_priv.h) being the latter
       only available for sdm845.
    
     * This results in the same as applying https://goo.gl/SR7sFY,
       but it's preferable since it makes use of our own method of
       extending the valid gralloc 1.0 usage bits (see commit 59009f8).
       TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS is extended here on
       purpose and as a bitwise OR of all the values in order not to
       clear what might have been set in the device trees.
    
    Change-Id: I5536f341e75088cc72ac369065011388f0a6d3f1

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -13,7 +13,12 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += guaiyihuGlobalVars
 SOONG_CONFIG_guaiyihuGlobalVars += \
+    additional_gralloc_10_usage_bits \
     target_process_sdk_version_override
 
+# Set default values
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
+
 # Soong value variables
+SOONG_CONFIG_guaiyihuGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_guaiyihuGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
