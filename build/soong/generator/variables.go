BenzoRom/build_soong

Dynamically generate kernel headers using generator
Add a soong vendor plugin for kernel config variables so we can use
these in go.

Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func benzoExpandVariables(ctx android.ModuleContext, in string) string {
+	benzoVars := ctx.Config().VendorConfig("benzoVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if benzoVars.IsSet(name) {
+			return benzoVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}

tiger-cave/vendor_guaiyihu

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func guaiyihuExpandVariables(ctx android.ModuleContext, in string) string {
+	guaiyihuVars := ctx.Config().VendorConfig("guaiyihuVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if guaiyihuVars.IsSet(name) {
+			return guaiyihuVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}